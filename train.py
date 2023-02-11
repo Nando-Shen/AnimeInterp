c05f1ddecdd35a2d3e9f65ab84a266fad4c7a381
shenjiaming <shenjiaming@rd.netease.com>
2023-02-11 23:26:17 +0800

    trainning

--- train.py
+++ train.py
@@ -98,7 +98,7 @@ def train(config):
     ssim_whole = 0
     ssims = np.zeros([len(testset), config.inter_frames])
     # losses, psnrs, ssims = myutils.init_meters(config.loss)
-    folders = []
+    # folders = []
 
     print('Everything prepared. Ready to train...')
     sys.stdout.flush()
@@ -109,8 +109,8 @@ def train(config):
     torch.cuda.empty_cache()
 
     for validationIndex, validationData in enumerate(trainloader, 0):
-        print('Training {}/{}-th group...'.format(validationIndex, len(testset)))
-        sys.stdout.flush()
+        if (validationIndex % 200 == 0):
+            print('Training {}/{}-th group...'.format(validationIndex, len(testset)))
         sample, flow = validationData
 
         frame0 = None
@@ -175,46 +175,16 @@ def train(config):
     return None
 
 
-def validate(config):   
-    # preparing datasets & normalization
-    normalize1 = TF.Normalize(config.mean, [1.0, 1.0, 1.0])
-    normalize2 = TF.Normalize([0, 0, 0], config.std)
-    trans = TF.Compose([TF.ToTensor(), normalize1, normalize2, ])
-
-    revmean = [-x for x in config.mean]
-    revstd = [1.0 / x for x in config.std]
-    revnormalize1 = TF.Normalize([0.0, 0.0, 0.0], revstd)
-    revnormalize2 = TF.Normalize(revmean, [1.0, 1.0, 1.0])
-    revNormalize = TF.Compose([revnormalize1, revnormalize2])
-
-    revtrans = TF.Compose([revnormalize1, revnormalize2, TF.ToPILImage()])
-
-    testset = datas.AniTripletWithSGMFlowTest(config.testset_root, config.test_flow_root, trans, config.test_size, config.test_crop_size, train=False)
-    sampler = torch.utils.data.SequentialSampler(testset)
-    validationloader = torch.utils.data.DataLoader(testset, sampler=sampler, batch_size=1, shuffle=False, num_workers=1)
-    to_img = TF.ToPILImage()
- 
-    print(testset)
-    sys.stdout.flush()
-
-    # prepare model
-    model = getattr(models, config.model)(config.pwc_path).cuda()
-
-    # load weights
-    dict1 = torch.load(config.checkpoint)
-    model.load_state_dict(dict1['model_state_dict'], strict=False)
-
-    # prepare others
-    store_path = config.store_path
+def validate(config):
 
     ## values for whole image
-    # psnr_whole = 0
-    # psnrs = np.zeros([len(testset), config.inter_frames])
-    # ssim_whole = 0
-    # ssims = np.zeros([len(testset), config.inter_frames])
-    losses, psnrs, ssims = myutils.init_meters(config.loss)
+    psnr_whole = 0
+    psnrs = np.zeros([len(testset), config.inter_frames])
+    ssim_whole = 0
+    ssims = np.zeros([len(testset), config.inter_frames])
+    # losses, psnrs, ssims = myutils.init_meters(config.loss)
 
-    folders = []
+    # folders = []
 
     print('Everything prepared. Ready to test...')  
     sys.stdout.flush()
@@ -237,7 +207,7 @@ def validate(config):
             frame3 = None
             frame2 = sample[-1]
 
-            folders.append(folder[0][0])
+            # folders.append(folder[0][0])
             
             # initial SGM flow
             F12i, F21i  = flow
@@ -248,13 +218,13 @@ def validate(config):
             ITs = [sample[tt] for tt in range(1, 2)]
             I1 = frame1.cuda()
             I2 = frame2.cuda()
-            
-            if not os.path.exists(config.store_path + '/' + folder[0][0]):
-                os.mkdir(config.store_path + '/' + folder[0][0])
+
+            # if not os.path.exists(config.store_path + '/' + folder[0][0]):
+            #     os.mkdir(config.store_path + '/' + folder[0][0])
 
 
-            revtrans(I1.cpu()[0]).save(store_path + '/' + folder[0][0] + '/'  + index[0][0] + '.jpg')
-            revtrans(I2.cpu()[0]).save(store_path + '/' + folder[-1][0] + '/' +  index[-1][0] + '.jpg')
+            # revtrans(I1.cpu()[0]).save(store_path + '/' + folder[0][0] + '/'  + index[0][0] + '.jpg')
+            # revtrans(I2.cpu()[0]).save(store_path + '/' + folder[-1][0] + '/' +  index[-1][0] + '.jpg')
             for tt in range(config.inter_frames):
                 x = config.inter_frames
                 t = 1.0/(x+1) * (tt + 1)
@@ -263,48 +233,49 @@ def validate(config):
 
                 It_warp = outputs[0]
 
-                to_img(revNormalize(It_warp.cpu()[0]).clamp(0.0, 1.0)).save(store_path + '/' + folder[1][0] + '/' + index[1][0] + '.png')
+                # to_img(revNormalize(It_warp.cpu()[0]).clamp(0.0, 1.0)).save(store_path + '/' + folder[1][0] + '/' + index[1][0] + '.png')
 
                 estimated = revNormalize(It_warp[0].cpu()).clamp(0.0, 1.0).numpy().transpose(1, 2, 0)
                 gt = revNormalize(ITs[tt][0]).clamp(0.0, 1.0).numpy().transpose(1, 2, 0) 
 
                 # whole image value
-                # this_psnr = compare_psnr(estimated, gt)
-                # this_ssim = compare_ssim(estimated, gt, multichannel=True, gaussian=True)
+                this_psnr = psnr(estimated, gt)
+                this_ssim = ssim(estimated, gt, multichannel=True, gaussian=True)
                 #
                 # psnrs[validationIndex][tt] = this_psnr
                 # ssims[validationIndex][tt] = this_ssim
                 #
-                # psnr_whole += this_psnr
-                # ssim_whole += this_ssim
+                psnr_whole += this_psnr
+                ssim_whole += this_ssim
                 loss, _ = criterion(estimated, gt)
-                losses['total'].update(loss.item())
+                # losses['total'].update(loss.item())
                 myutils.eval_metrics(estimated, gt, psnrs, ssims)
 
-        # psnr_whole /= (len(testset) * config.inter_frames)
-        # ssim_whole /= (len(testset) * config.inter_frames)
+        psnr_whole /= (len(testset) * config.inter_frames)
+        ssim_whole /= (len(testset) * config.inter_frames)
 
-
-    return losses['total'].avg, psnrs.avg, ssims.avg, folders
+    return psnr_whole, ssim_whole
 
 
 if __name__ == "__main__":
 
     for epoch in range(0, config.max_epoch):
-        train(config)
 
+        # train(config)
+        psnr, ssim = validate(config)
         #torch.save()
+        # print('PSNR is {}'.format(psnr))
+        # print('SSIM is {}'.format(ssim))
+        print('Epoch [{0}/{1}], Val_PSNR:{2:.2f}, Val_SSIM:{3:.4f}'
+              .format(epoch, config.max_epoch, psnr, ssim))
 
-
-    loss, psnr, ssim, folder = validate(config)
     # for ii in range(config.inter_frames):
     #     print('PSNR of validation frame' + str(ii+1) + ' is {}'.format(np.mean(psnrs[:, ii])))
     #
     # for ii in range(config.inter_frames):
     #     print('PSNR of validation frame' + str(ii+1) + ' is {}'.format(np.mean(ssims[:, ii])))
 
-    print('Whole PSNR is {}'.format(psnr) )
-    print('Whole SSIM is {}'.format(ssim) )
+
 
     # with open(config.store_path + '/psnr.txt', 'w') as f:
     #     for index in sorted(range(len(psnrs[:, 0])), key=lambda k: psnrs[k, 0]):
